821d44ca9454fd3314c7343f5a038c20f72440b5
superzbo <zbo2@qq.com>
2016-04-10 19:15:16 +0800

    add sb2 bootstrap template

---
+++ src/main/java/org/springframework/samples/petclinic/web/SBController.java
@@ -0,0 +1,61 @@
+package org.springframework.samples.petclinic.web;
+
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import com.google.gson.Gson;
+import com.mysql.fabric.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Owners;
+import org.springframework.samples.petclinic.model.Vets;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.util.JsonUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by zbo on 4/10/16.
+ */
+@Controller
+public class SBController {
+    private final ClinicService clinicService;
+
+    @Autowired
+    public SBController(ClinicService clinicService) {
+        this.clinicService = clinicService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    protected HttpServletRequest request;
+    protected HttpServletResponse response;
+    protected HttpSession session;
+
+    @ModelAttribute
+    public void setReqAndRes(HttpServletRequest request, HttpServletResponse response){
+        this.request = request;
+        this.response = response;
+        this.session = request.getSession();
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String initCreationForm(Map<String, Object> model) {
+        return "redirect:/sb2/index.html";
+    }
+
+}
